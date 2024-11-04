0a2eb1d9d78d8bb026f9d3e840944bfce04c33c2
jejumiso <jejumiso@gmail.com>
2024-11-04 17:02:00 +0900

    web.js cors 적용후

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 // web.js
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // CORS 미들웨어 추가
 const aligoRoutes = require('./routes/aligo');
 
 const app = express();
 const PORT = 8001;
+
+// CORS 설정: 모든 도메인에서의 요청 허용
+app.use(cors());
 
 // JSON 요청 파싱 설정
 app.use(bodyParser.json());
