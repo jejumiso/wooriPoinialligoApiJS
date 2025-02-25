8332bbe07dade0d0494c5ed58bdba818ecf71a7e
jejumiso <jejumiso@gmail.com>
2025-02-25 14:00:05 +0900

    fix

--- handlers/bootpayHandlers.js
+++ handlers/bootpayHandlers.js
@@ -26,7 +26,11 @@ exports.requestUserToken = async (req, res) => {
                 private_key = "Gu35HLZCQ8Sqj7QAVZSXWESGNgbx6jG2TOIQqFIVh6A=";
             }
         }
+
+
         if (companyId == "topping-up") {
+
+
             if (isReal) {
                 application_id = "6760f9414fb27baaf86e522d";
                 private_key = "pzWm9qcLC/HfHV71/PdDfloRKiuxjZhEYMT9Qol33H0=";
@@ -35,10 +39,15 @@ exports.requestUserToken = async (req, res) => {
                 private_key = "WfOtF7Fn9RzM0hDK+T8OiHpyhvngazPbsd7LPlqGxMU=";
             }
         }
+
+
         if (companyId == "jerajune_duty_free") {
             application_id = "67b5418986fd08d2213fcd55";
             private_key = "x0sYAt86UH7IqJEKBDqlgKtubzn1lLPCsro3I98vdQ4=";
         }
+
+
+
         console.log("간변비밀번호 token발행 시작   application_id : " + application_id);
         console.log("간변비밀번호 token발행 시작   private_key : " + private_key);
         //서버에서 갖고와야 하지만 임시... END
