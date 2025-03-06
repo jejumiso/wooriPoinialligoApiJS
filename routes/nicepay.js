351cc0e8646bb826970a4a45fa78658a6a242521
jejumiso <jejumiso@gmail.com>
2025-03-06 11:22:22 +0900

    fix

--- routes/nicepay.js
+++ routes/nicepay.js
@@ -5,6 +5,7 @@ const { responseEndpoint } = require('../handlers/nicepayHandlers');
 
 // 부트페이 관련 API 경로
 router.post('/responseEndpoint', responseEndpoint);
+router.get('/responseEndpointget', responseEndpoint);
 
 module.exports = router;
 
