54323a4edfcf307839cc221160f6539b3ea3594a
jejumiso <jejumiso@gmail.com>
2025-03-08 04:33:28 +0900

    fix

--- routes/nicaPay.js
+++ routes/nicaPay.js
@@ -1,12 +1,8 @@
-// routes/nicepay.js
+// routes/nicaPay.js
 const express = require('express');
 const router = express.Router();
-const { responseEndpoint, handleNicepayWebhook, payments } = require('../handlers/nicePayHandlers');
+const { payments } = require('../handlers/nicePayHandlers');
 
-// 나이스페이 관련 API 경로
-router.post('/responseEndpoint', responseEndpoint);
-router.get('/responseEndpoint', responseEndpoint);
-router.post('/nicepayWebhook', handleNicepayWebhook);
 router.post('/payments', payments);
 
 module.exports = router;
