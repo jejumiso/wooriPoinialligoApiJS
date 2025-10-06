e43290c00c7575453b7e11a2d6e7f954d35884cc
jejumiso <jejumiso@gmail.com>
2025-10-06 15:09:36 +0900

    테스트 암호화,복호화 키,iv

--- handlers/resHandlers.js
+++ handlers/resHandlers.js
@@ -2,14 +2,14 @@
 const aligoapi = require('aligoapi');
 const AuthData = require('../config/auth');
 
-const key = 'QRvUJwa5cIubaa9QHLahkizjCNyKfC76';
-const iv = 'QU83jDdX2PrZNL7k';
+// const key = 'QRvUJwa5cIubaa9QHLahkizjCNyKfC76';
+// const iv = 'QU83jDdX2PrZNL7k';
 
 
 const { encryptData, decryptData } = require('../utils/encryption.js');
 
 const resEncoDeco = (req, res) => {
-    const { oriText, resText } = req.body; // req.body에서 ori와 res를 추출
+    const { oriText, resText,key,iv } = req.body; // req.body에서 ori와 res를 추출
     // 암호화
     const resultResFromOri = encryptData(oriText, key, iv);
     // URL-safe 
